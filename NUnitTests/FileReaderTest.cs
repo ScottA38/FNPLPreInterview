7bc20b2f78704bc4a6837a287828b2456da7821a
ScottA94 <94andersonsc@googlemail.com>
2021-11-07 17:14:41 +0200

    master
    	- Added FileReader unit tests

---
+++ NUnitTests/FileReaderTest.cs
@@ -0,0 +1,66 @@
+﻿
+using NUnit.Framework;
+using System;
+using System.IO;
+using FNPLPreInteview;
+
+namespace Tests
+{
+    [TestFixture()]
+    public class FileReaderTest
+    {
+        protected FileReader sampleFileReader;
+
+        protected FileReader errorFileReader;
+
+        protected readonly string validBasePath = "files";
+
+        protected readonly string validFileName = "test";
+
+        [SetUp()]
+        public void init()
+        {
+            sampleFileReader = new FileReader(validFileName, validBasePath);
+            try
+            {
+                errorFileReader = new FileReader("invalid", "invalid");
+            } catch { }
+        }
+
+        [Test()]
+        public void constructorAcceptsValidFilenameAndBasePath()
+        {
+            Assert.DoesNotThrow(
+                () => { new FileReader(validFileName, validBasePath); }
+            );
+        }
+
+        [Test()]
+        public void constructorDoesNotAcceptInvalidFileName()
+        {
+            Assert.Throws<FileLoadException>(
+                () => { new FileReader("invalid", validBasePath); }
+            );
+        }
+
+        [Test()]
+        public void constructorDoesNotAcceptInvalidBasePath()
+        {
+            Assert.Throws<FileLoadException>(
+                () => { new FileReader(validFileName, "invalid"); }
+            );
+        }
+
+        [Test()]
+        public void providesFileContentsForValidPath()
+        {
+            Assert.IsInstanceOf<string>(sampleFileReader.FileContents);
+        }
+
+        [Test()]
+        public void providesNullFileContentsForInvalidPath()
+        {
+            Assert.IsNull(errorFileReader.FileContents);
+        }
+    }
+}
